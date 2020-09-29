34db3f04e011dd3c3ae5dd86045db575525ec93d
Ana Sousa Silva de Sousa <ss.anaclara@ibm.com>
2020-09-29 11:57:22 -0300

    initializing project

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
+// import PrivateRoute from '../../authentication/PrivateRoute';
+// import PublicRoute from '../../authentication/PublicRoute';
+// import { Provider } from 'react-redux';
+// import { Store } from '../../store/index';
+// import Chat from '../Chat/Chat';
+function App() {
+  return (<h1>oi</h1>);
+}
+export default App;
