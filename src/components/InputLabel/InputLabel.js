418bc0018a2ce03da2ca2c49c67f7fc43df505f0
Ana Sousa Silva de Sousa <ss.anaclara@ibm.com>
2020-10-02 12:38:11 -0300

    initial components

---
+++ src/components/InputLabel/InputLabel.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.scss';
+// import App from './containers/App/App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+import React, { Component } from 'react'
+import './InputLabel.scss'
+export default class InputLabel extends Component {
+  render() {
+    return (
+      <div className='container-inputlabel'>
+        <label>{this.props.label}</label>
+        <div className="container-input-icon">
+          <input className="input" onChange={(e) => this.props.callback(e.target.value)} type={this.props.type} />
+          {this.props.icon && this.props.icon}
+        </div>
+      </div>
+    )
+  }
+}
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.scss';
-import {
-  BrowserRouter as Router,
-  Switch,
-} from "react-router-dom";
+import Login from '../Login/Login'
+import { BrowserRouter, Route } from 'react-router-dom';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+// } from "react-router-dom";
 // import PrivateRoute from '../../authentication/PrivateRoute';
 // import PublicRoute from '../../authentication/PublicRoute';
 // import { Provider } from 'react-redux';
 // import { Store } from '../../store/index';
 // import Chat from '../Chat/Chat';
 function App() {
-  return (<h1>oi</h1>);
+  return (
+    <BrowserRouter>
+      <Route path='/' exact component={Login} />
+    </BrowserRouter>
+  );
 }
 export default App;
