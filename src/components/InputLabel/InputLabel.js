48c0a4cc8c0be269f86928b3155ca911b00de325
Ana Sousa Silva de Sousa <ss.anaclara@ibm.com>
2020-10-05 16:35:12 -0300

    button component

--- src/components/InputLabel/InputLabel.js
+++ src/components/InputLabel/InputLabel.js
@@ -10,18 +10,17 @@
 // // unregister() to register() below. Note this comes with some pitfalls.
 // // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
-import React, { Component } from 'react'
-import './InputLabel.scss'
-export default class InputLabel extends Component {
-  render() {
-    return (
-      <div className='container-inputlabel'>
-        <label>{this.props.label}</label>
-        <div className="container-input-icon">
-          <input className="input" onChange={(e) => this.props.callback(e.target.value)} type={this.props.type} />
-          {this.props.icon && this.props.icon}
-        </div>
-      </div>
-    )
-  }
-}
+// import "../InputLabel"
+// export default class InputLabel extends Component {
+//   render() {
+//     return (
+//       <div className='container-inputlabel'>
+//         <label>{this.props.label}</label>
+//         <div className="container-input-icon">
+//           <input className="input" onChange={(e) => this.props.callback(e.target.value)} type={this.props.type} />
+//           {this.props.icon && this.props.icon}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -3,10 +3,11 @@
 //     return ( <h1>justin bieber lixo</h1>)}
 import React, { Component } from "react";
 // import * as authentication from "../../providers/authentication";
-import InputLabel from "../../components/InputLabel/InputLabel";
+import AccessButton from "../../components/Button/AccessButton"
 
 // import Button from "../../components/Button";
 // import { Redirect } from "react-router-dom";
+// import  Button  from "../../components/Button/Button";
 import "../Login/Login.scss";
 const logoNZN = require("../../assets/images/logoNZN.png");
 
@@ -17,13 +18,14 @@ function Login() {
     <div className="container-login">
       <span className="banner">
         <p>HELP</p>
+        <AccessButton></AccessButton>
       </span>
       <div className="box-form">
         <img className="logo" alt="logo" src={logoNZN} />
         <h1>Login</h1>
         <form>
-          <InputLabel label="Email or Phone Number" />
-          <InputLabel label="Password" />
+          {/* <InputLabel label="Email or Phone Number" />
+          <InputLabel label="Password" /> */}
         </form>
       </div>
     </div>
