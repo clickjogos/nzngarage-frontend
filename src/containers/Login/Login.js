a4288a061e07176e3c6441c6bbcc78fe51716a52
fernandasnuness <fernanda.nunes@ibm.com>
2020-10-06 11:17:23 -0300

    adjust variables and page login/ adjust font

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -27,6 +27,8 @@ function Login() {
         </div>
       </div>
       <div class="background-information">
+      <h1>Mensurar o conhecimento e estimular o engajamento nunca foi tão simples</h1>
+
         <div class="containerImage">
           <Image src={backgroundLogin} height="544" width="727" />
         </div>
