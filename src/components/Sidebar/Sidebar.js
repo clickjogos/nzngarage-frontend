7fee69646916e88b5a7f49fefd588a2b6add5b3f
fernandasnuness <fernanda.nunes@ibm.com>
2020-10-06 17:07:31 -0300

    added sidebar

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./Sidebar.scss";
+import Image from "react-bootstrap/Image";
+const planning = require("../../assets/icons/icon-planejamento.svg");
+const acompanhamento = require("../../assets/icons/icon-acompanhamento.svg");
+const logoNZN = require("../../assets/images/nzn-logo.png");
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div class="sidebar">
+        <div class="logo">
+          <Image src={logoNZN} height="44px" width="173px" />
+        </div>
+        <hr />
+        <div class="icon-container">
+          <p id="txtplan">CRIAR</p>
+          <button
+            className="buttonSidebar"
+            onClick={() => this.props.callback()}
+              >
+            <img id="imgIcon"src={planning} height="16px" width="14px" />
+            <p id="txtButtonPlan">Planejamento</p>
+          </button>
+          <br></br>
+
+          <p id="txtplan">AVALIAR</p>
+          <button
+            className="buttonSidebar"
+            onClick={() => this.props.callback()}
+              >
+            <img id="imgIcon"src={acompanhamento} height="16px" width="14px" />
+            <p id="txtButtonGuide">Acompanhamento</p>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/containers/CreatePlanning/CreatePlanning.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Image from "react-bootstrap/Image";
+
+import Sidebar from "../../components/Sidebar/Sidebar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "../Login/Login.scss";
+
+function CreatePanning() {
+  return (
+    <div class="main">
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default CreatePanning;
