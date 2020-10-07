066bee0f37425874641129204703e5861188ef0d
fernandasnuness <fernanda.nunes@ibm.com>
2020-10-07 15:36:55 -0300

    building planning page

--- src/containers/CreatePlanning/CreatePlanning.js
+++ src/containers/CreatePlanning/CreatePlanning.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import ButtonBack from "../../components/Button/Back";
-
 import Sidebar from "../../components/Sidebar/Sidebar";
-
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import InputLabelPlanning from "../../components/InputLabel/InputLabelPlanning";
 import "./CreatePlanning.scss";
 const Backbutton = require("../../assets/icons/icon-back-button.svg");
 
@@ -12,22 +10,24 @@ function CreatePanning() {
   return (
     <div class="main">
       <Sidebar />
-    <div class="container-block">
-    <div class="container-back">
-      <button>
-        <img src={Backbutton} icon={<img src={Backbutton} />} 
-        />
-      </button>
-      <p id="back-text"> Voltar para a Lista de Planejamento</p>
-    </div>
-    
-    <div class="teste">
-      <h3>Criar novo Planejamento</h3>
-      <h4>Vamos fazer isso em dois passos ;)</h4>
-      </div>
-      </div>
-      </div>
+      <div class="container-block">
+        <div class="container-back">
+          <button>
+            <img src={Backbutton} icon={<img src={Backbutton} />} />
+          </button>
+          <p id="back-text"> Voltar para a Lista de Planejamento</p>
+        </div>
 
+        <div class="container-planning">
+          <h3 style={{ fontSize: "28px" }}>Criar novo Planejamento</h3>
+          <h4 style={{ fontSize: "18px", color: "#636F7A" }}>
+            Vamos fazer isso em dois passos ;)
+          </h4>
+          <InputLabelPlanning label="Nome do Planejamento" placeholder="Mês de Outubro"/>
+          <InputLabelPlanning label="Data Inicial" placeholder="Senha de acesso"/>
+        </div>
+      </div>
+    </div>
   );
 }
 
