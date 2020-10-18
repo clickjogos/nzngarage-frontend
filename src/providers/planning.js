a21ad78bc7fe7afa3c14f256b1c742d4070d2178
regiany.nunes@ibm.com <regiany.nunes@ibm.com>
2020-10-17 21:19:21 -0300

    merge with 'fernanda' branch: adding edit planning

--- src/providers/planning.js
+++ src/providers/planning.js
@@ -8,3 +8,6 @@ export function savePlanning(object) {
     return axiosProvider.post('/planning/', object);
 }
 
+export function getPlanning(payload) {
+    return axiosProvider.get('/planning/'+payload.id );
+}
