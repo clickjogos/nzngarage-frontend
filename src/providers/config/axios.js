e8e660871849692bdb84526a77e4e77c57f6ce27
Thiago Lobo <15thiagolobo@gmail.com>
2020-10-20 10:25:44 -0300

    https correct minor

--- src/providers/config/axios.js
+++ src/providers/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const PROD = 'http://nzn-garage-cluster-316ee121d9b40536dfeb045d2e84c949-0000.sjc03.containers.appdomain.cloud/api'
+const PROD = 'https://nzn-garage-cluster-316ee121d9b40536dfeb045d2e84c949-0000.sjc03.containers.appdomain.cloud/api'
 
 const axiosProvider = axios.create({
   baseURL: PROD
