d8118f9ed1470126e5bcc41ee51070c740e7e3c3
Thiago Lobo <15thiagolobo@gmail.com>
2020-10-23 09:46:17 -0300

    css adjustments w dev

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -50,43 +50,43 @@ export default class Cards extends Component {
 
 	checkConditions = (props) => {
 		let percentage = parseInt((this.state.realPoints * 100) / this.state.plannedPoints)
-		
+
 		if (percentage >= 100) {
-			this.setState({ progressBarWidth: { width: '100%' }, show: true }, (e) => {})
+			this.setState({ progressBarWidth: { width: '100%' }, show: true }, (e) => { })
 		} else {
 			this.setState({ progressBarWidth: { width: `${percentage}%` }, show: true }, (e) => {
-				
+
 			})
 		}
 	}
 
 	verifyAlerts() {
 		// console.log("x da questao")
-		let marginMarker = parseInt((this.state.expectedPoints * 100) / this.state.plannedPoints)		
+		let marginMarker = parseInt((this.state.expectedPoints * 100) / this.state.plannedPoints)
 		if (isHead()) {
 			// console.log('lets verify alerts')
 			// console.log(marginMarker)
 			if (marginMarker >= 100) marginMarker = 100
 			// console.log(marginMarker)
-			this.setState({expectedBarWidth: { 'left': `${marginMarker}%` } }, () => {
+			this.setState({ expectedBarWidth: { 'left': `${marginMarker}%` } }, () => {
 				// console.log("leeeeft",this.state.expectedBarWidth)
 				if (this.state.order === 'asc') {
 					// console.log('it is asc')
 					if (this.state.expectedPoints >= this.state.realPoints) {
-						this.setState({  alert: true,  spanAlertText: 'Você está abaixo do esperado!' })
+						this.setState({ alert: true, spanAlertText: 'Você está abaixo do esperado!' })
 						// this.props.onAlert()
 					}
 				} else if (this.state.order === 'desc') {
 					// console.log('it is desc')
 					if (this.state.expectedPoints <= this.state.realPoints) {
-						this.setState({  alert: true,  spanAlertText: 'Você está acima do esperado!' })
+						this.setState({ alert: true, spanAlertText: 'Você está acima do esperado!' })
 						// this.props.onAlert
 					}
 				}
 			})
 		}
 	}
-	
+
 	render() {
 		return this.state.show ? (
 			<div className="card">
@@ -99,28 +99,29 @@ export default class Cards extends Component {
 					</div>
 					<div>
 						{this.state.alert ? (
-							
-							
+
+
 							<a>
 								{' '}
 								!!! <span>{this.state.spanAlertText}</span>{' '}
 							</a>
-							
+
 						) : (
-							<></>
-						)}
+								<></>
+							)}
 					</div>
 				</div>
-				<div className="progressBar">
-					<div style={this.state.progressBarWidth} className="percentageItem"></div>
-					{isHead() ? (<div  style={this.state.expectedBarWidth} className="expectedItem">
-						<p  className="expectedValue">{this.state.expectedPoints}</p>
-						<p className="expectedMark">{''}</p></div>) : (<></>)} 
-					
+				<div className="progress-information">
+					<div className="progressBar">
+						<div style={this.state.progressBarWidth} className="percentageItem"></div>
+						{isHead() ? (<div style={this.state.expectedBarWidth} className="expectedItem">
+							<p className="expectedValue">{this.state.expectedPoints}</p>
+							<p className="expectedMark">{''}</p></div>) : (<></>)}
+					</div>
 				</div>
 			</div>
 		) : (
-			<Loading />
-		)
+				<Loading />
+			)
 	}
 }
